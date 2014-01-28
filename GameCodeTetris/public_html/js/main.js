6a2dd787a170a5e7576607ff6d13c869b2f52960
ArtGames <sokol89@outlook.com>
2014-01-28 08:50:23 +0200

    Работа идет

--- GameCodeTetris/public_html/js/main.js
+++ GameCodeTetris/public_html/js/main.js
@@ -1,7 +1,15 @@
 $(document).ready( function (){
 	$('#gamearea').click(function(){
 	var HTML =  $('#html').val();
-  $('#gamearea').html(HTML); 
+    $('#gamearea').html(HTML);
+    $('#gamearea div').animate({left:'250px'});
+////    $('div').animate ({
+////     position:'absolute',
+////     top: '100px'
+//// });
 	});
+     
 });
-
+function MoveBlock (){
+  $(#)  
+}
