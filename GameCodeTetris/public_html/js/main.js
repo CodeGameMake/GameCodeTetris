72cf07da447c78746666b610d04104219a97f46c
ArtGames <sokol89@outlook.com>
2014-02-02 15:54:20 +0200

    Little correction

--- GameCodeTetris/public_html/js/main.js
+++ GameCodeTetris/public_html/js/main.js
@@ -1,15 +1,12 @@
 $(document).ready( function (){
-	$('#gamearea').click(function(){
-	var HTML =  $('#html').val();
+   $('#gamearea').click(function(){
+    var HTML =  $('#html').val();
     $('#gamearea').html(HTML);
     $('#gamearea div').animate({left:'250px'});
-////    $('div').animate ({
-////     position:'absolute',
-////     top: '100px'
-//// });
-	});
-     
+   });    
 });
 function MoveBlock (){
-  $(#)  
+    $('#gamearea div').click(function(){
+            var Block
+    });
 }
