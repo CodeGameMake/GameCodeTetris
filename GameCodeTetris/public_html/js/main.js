2e6057a7d6553eaf384c973d3a12093b4cb60170
Nikola_xiii <nikola.serdiuk@gmail.com>
2014-02-16 16:14:43 +0200

    Added slow fall block and Function LeftRandom

--- GameCodeTetris/public_html/js/main.js
+++ GameCodeTetris/public_html/js/main.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
 	$('.block').click(function () {
-		$('.block').animate({
-			top: '720px'
-		},"slow");
+		$('.block').css ('left', LeftRandom());
+		$('.block').animate({ top: '720px'},5000);
 	});
 });
+
+function LeftRandom()
+{
+	var pixel = Math.random()*620;
+	pixel = Math.round(pixel)+"px"
+	return pixel;
+}
