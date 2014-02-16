bd655f2eed895bb3ed9e9d2282684be49070f6f6
Nikola_xiii <nikola.serdiuk@gmail.com>
2014-02-16 18:28:54 +0200

    Added disabled in html, join input with FallBlock

--- GameCodeTetris/public_html/js/main.js
+++ GameCodeTetris/public_html/js/main.js
@@ -1,29 +1,35 @@
 $(document).ready(function () {
-    $('.block').click(function () {
-        $('.block').css('left', LeftRandom());
-        $('.block').animate({
-            top: '720px'
-        }, 5000);
-    });
-});
-$("#html").focusin(function () {
-    $("#html").keydown(function (event) {
-        if (event.which == "13") {
-            var text =  $("#html").val();
-            CreateBlock(text);
-        }
-    });
+	$("#html").focusin(function () {
+		$("#html").keydown(function (event) {
+			if (event.which == "13") {
+				var text = $("#html").val();
+				$('#html').attr('disabled', 'disabled');
+				CreateBlock(text);
+				FallBlock(5000);
+			}
+		});
+	});
 });
 
+function FallBlock(speed) {
+	$('.block').css('left', LeftRandom());
+	$('.block').animate({
+		top: '720px'
+	}, speed);
+
+}
+
+
 function CreateBlock(text) {
-    if (text == '<div></div>') {
-        $("#gamearea").html(text);
-    }
-    //  $("#gamearea").html(text);
+	if (text == '<div></div>') {
+		$("#gamearea").html(text);
+		$('#gamearea div').addClass('block');
+	}
+	//  $("#gamearea").html(text);
 }
 
 function LeftRandom() {
-    var pixel = Math.random() * 620;
-    pixel = Math.round(pixel) + "px"
-    return pixel;
+	var pixel = Math.random() * 620;
+	pixel = Math.round(pixel) + "px"
+	return pixel;
 }
