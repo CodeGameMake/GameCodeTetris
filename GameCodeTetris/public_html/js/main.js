bd7ae708d28c52eafe5be30ea6f5848608040910
ArtGames <sokol89@outlook.com>
2014-02-16 17:58:32 +0200

    Transfer code to gamearea

--- GameCodeTetris/public_html/js/main.js
+++ GameCodeTetris/public_html/js/main.js
@@ -9,10 +9,18 @@ $(document).ready(function () {
 $("#html").focusin(function () {
     $("#html").keydown(function (event) {
         if (event.which == "13") {
-            $("#html").attr('disabled');
+            var text =  $("#html").val();
+            CreateBlock(text);
         }
     });
 });
+
+function CreateBlock(text) {
+    if (text == '<div></div>') {
+        $("#gamearea").html(text);
+    }
+    //  $("#gamearea").html(text);
+}
 
 function LeftRandom() {
     var pixel = Math.random() * 620;
