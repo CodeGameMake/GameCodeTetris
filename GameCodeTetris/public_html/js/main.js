c6ea1a4d854afd559d9f2613886652df271d0ec3
ArtGames <sokol89@outlook.com>
2014-02-16 17:02:35 +0200

    Main.js

--- GameCodeTetris/public_html/js/main.js
+++ GameCodeTetris/public_html/js/main.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
-	$('.block').click(function () {
-		$('.block').css ('left', LeftRandom());
-		$('.block').animate({ top: '720px'},5000);
-	});
+    $('.block').click(function () {
+        $('.block').css('left', LeftRandom());
+        $('.block').animate({
+            top: '720px'
+        }, 5000);
+    });
+});
+$("#html").focusin(function () {
+    $("#html").keydown(function (event) {
+        if (event.which == "13") {
+            $("#html").attr('disabled');
+        }
+    });
 });
 
-function LeftRandom()
-{
-	var pixel = Math.random()*620;
-	pixel = Math.round(pixel)+"px"
-	return pixel;
+function LeftRandom() {
+    var pixel = Math.random() * 620;
+    pixel = Math.round(pixel) + "px"
+    return pixel;
 }
